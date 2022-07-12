7f548e5d7a7b22ac25eae5cba1f4421a561b9523
Andrew Reed <andrew.reed.r@gmail.com>
2022-07-12 11:21:48 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="text-style-transfer",
+    version="0.1.0",
+    description="A library and demo application for the NLP task of text style transfer.",
+    author="Andrew Reed",
+    author_email="andrew.reed.r@gmail.com",
+    packages=find_packages(),
+)
